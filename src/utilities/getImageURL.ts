0fcb3e84798c78d5c557043da22a9397291a48da
m-padilla <padillamarco18@gmail.com>
2025-10-06 15:20:01 -0700

    testing getImageURLs function.

--- src/utilities/getImageURL.ts
+++ src/utilities/getImageURL.ts
@@ -1,3 +1,4 @@
 export function getImageUrl(path: string){
+    console.log('https://dessert-shopping-cart.onrender.com' + path)
     return new URL('https://dessert-shopping-cart.onrender.com' + path, import.meta.url).href;
 }
