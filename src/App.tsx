70dce786769730115f3172997308f3590ddaca6e
m-padilla <padillamarco18@gmail.com>
2025-10-16 14:07:10 -0700

    added loading skeletions for desserts cards.

--- src/App.tsx
+++ src/App.tsx
@@ -33,16 +33,16 @@ function App() {
 
   return (
     <>
-      {dessertsLoading ? (
-        <div>Loading...</div>
-      ) : dessertError ? (
-        <div>{dessertError.message}</div>
-      ) : (
-        <ShoppingCartProvider>
-          <div className="h-full w-full lg:p-10 bg-rose-50 md:flex ">
-            <div className='w-full lg:w-2/3'>
-              <div className='md:flex items-center'>
-                <h1 className='text-rose-900 text-5xl p-5 font-bold'>Desserts</h1>
+      <ShoppingCartProvider>
+        <div className="h-full w-full lg:p-10 bg-rose-50 md:flex ">
+          <div className='w-full lg:w-2/3'>
+            <div className='md:flex items-center'>
+              <h1 className='text-rose-900 text-5xl p-5 font-bold'>Desserts</h1>
+              {dessertsLoading ? (
+                <div className='md:ml-auto ml-5 px-5 h-10 w-fit bg-white text-rose-900 rounded-lg'>Loading...</div>
+              ) : dessertError ? (
+                <div>{dessertError.message}</div>
+              ) : (
                 <select
                   onChange={(e) => {
                     const category = e.target.value;
@@ -60,33 +60,70 @@ function App() {
                     <option key={category} value={category}>{category}</option>
                   ))}
                 </select>
+              )}
+            </div>
+            {dessertsLoading ? (
+              <div className='grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-3'>
+                {Array.from({ length: 3 }).map((_, i) => (
+                  <LoadingPlaceholder key={i} />
+                ))}
               </div>
+            ) : dessertError ? (
+              <div>{dessertError.message}</div>
+            ) : (
               <div className='grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-3'>
                 {selected && selected.map((dessert) => (
                   <DessertCard key={dessert._id} dessert={dessert} />
                 ))}
               </div>
-            </div>
-            <div className='md:w-1/3'>
-              <Cart />
-            </div>
+            )}
+
           </div>
-          <div className='text-center  text-rose-900 bg-rose-50 p-5'>
-            Challenge by <a
-              href="https://www.frontendmentor.io?ref=challenge"
-              target='_blank'
-              className='text-blue-700 underline hover:text-rose-800'
-            >Frontend Mentor</a>.
-            Coded by <a
-              href="https://www.marcoapadilla.com/"
-              target='_blank'
-              className='text-blue-700 underline hover:text-rose-800'
-            >Marco Padilla</a>
+          <div className='md:w-1/3'>
+            <Cart />
           </div>
-        </ShoppingCartProvider>
-      )}
+        </div>
+        <div className='text-center  text-rose-900 bg-rose-50 p-5'>
+          Challenge by <a
+            href="https://www.frontendmentor.io?ref=challenge"
+            target='_blank'
+            className='text-blue-700 underline hover:text-rose-800'
+          >Frontend Mentor</a>.
+          Coded by <a
+            href="https://www.marcoapadilla.com/"
+            target='_blank'
+            className='text-blue-700 underline hover:text-rose-800'
+          >Marco Padilla</a>
+        </div>
+      </ShoppingCartProvider>
+
     </>
   );
+}
+
+function LoadingPlaceholder() {
+  return (
+    <div className="p-3 ">
+      {/* image and button */}
+
+      <div className="w-full h-52 rounded-lg bg-slate-500 animate-pulse" />
+
+      {/* text info of desserts */}
+      <div className="py-7 space-y-2">
+
+        {/* category */}
+        <p className="max-w-24 bg-slate-500 animate-pulse h-4"></p>
+
+        {/* name */}
+        <p className="max-w-60  h-4 bg-slate-500 animate-pulse"></p>
+
+        {/* price */}
+        <p className="max-w-20 h-4 bg-slate-500 animate-pulse"></p>
+
+      </div>
+
+    </div>
+  )
 }
 
 export default App
--- src/Components/Cart.tsx
+++ src/Components/Cart.tsx
@@ -26,84 +26,86 @@ function Cart() {
     }))
 
     return (
+
+
         <>
-            {
-                loading ? <div>Loading...</div>
-                    : error ? <div>Error: {error.message}</div>
-                        : (
-                            <>
-                                <DialogModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
+            <DialogModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
 
-                                <div className='p-5 sticky'>
-                                    <div className='bg-white rounded-lg'>
-                                        <div className="flex pt-5 px-5 font-bold text-lg text-red">
-                                            <p>Your Cart</p>
-                                            <p className="pl-1">({cartQuantity})</p>
+            <div className='p-5 sticky'>
+                <div className='bg-white rounded-lg'>
+                    <div className="flex pt-5 px-5 font-bold text-lg text-red">
+                        <p>Your Cart</p>
+                        <p className="pl-1">({cartQuantity})</p>
+                    </div>
+
+                    {cartQuantity == 0
+                        ? <div className="flex flex-col justify-center items-center py-5">
+                            <img
+                                src={getImageUrl('/images/illustration-empty-cart.svg')}
+                                className="h-full aspect-square object-cover" />
+                            <span className="text-sm">Your added items will appear here</span>
+                        </div> : <>
+                            {
+                                cartItems.map(item => (
+                                    <CartItem id={item.id} key={item.id} />
+                                ))
+                            }
+                            <div className="px-5 pb-5">
+                                {/* Gross total */}
+                                <div className="flex">
+                                    <p className="text-md">Order Total</p>
+                                    {
+                                        loading ? (<div className="font-bold ml-auto text-lg">0.00</div>)
+                                            : error ? (<div>Error: {error.message}</div>)
+                                                : (
+                                                    <span className="font-bold ml-auto text-lg">
+                                                        {
+                                                            formatCurrency(
+                                                                cartItems.reduce((total, cartItem) => {
+                                                                    const item = desserts && desserts.find(dessert => dessert._id === cartItem.id)
+                                                                    return total + (item && item.price || 0) * cartItem.quantity;
+                                                                }, 0)
+                                                            )
+
+                                                        }
+                                                    </span>
+                                                   
+                                                )}
+
+                                </div>
+                                {/* little banner for the eco stuff */}
+                                <div className="py-3 text-center text-xs md:text-base">
+                                    <div className="bg-rose-50 p-2 rounded-md">
+                                        <div className="flex justify-center items-center">
+                                            <img
+                                                src={getImageUrl('/images/icon-carbon-neutral.svg')}
+                                                className="h-full aspect-square object-cover" />
+                                            <p className="pl-2">
+                                                This is a
+                                            </p>
+                                            <p className="font-bold px-1">
+                                                carbon-neutral
+                                            </p>
+                                            <p>
+                                                delivery
+                                            </p>
                                         </div>
-
-                                        {cartQuantity == 0
-                                            ? <div className="flex flex-col justify-center items-center py-5">
-                                                <img
-                                                    src={getImageUrl('/images/illustration-empty-cart.svg')}
-                                                    className="h-full aspect-square object-cover" />
-                                                <span className="text-sm">Your added items will appear here</span>
-                                            </div> : <>
-                                                {
-                                                    cartItems.map(item => (
-                                                        <CartItem id={item.id} key={item.id} />
-                                                    ))
-                                                }
-                                                <div className="px-5 pb-5">
-                                                    {/* Gross total */}
-                                                    <div className="flex">
-                                                        <p className="text-md">Order Total</p>
-                                                        <span className="font-bold ml-auto text-lg">
-                                                            {
-                                                                formatCurrency(
-                                                                    cartItems.reduce((total, cartItem) => {
-                                                                        const item = desserts && desserts.find(dessert => dessert._id === cartItem.id) 
-                                                                        return total + (item && item.price || 0) * cartItem.quantity;
-                                                                    }, 0)
-                                                                )
-
-                                                            }
-                                                        </span>
-                                                    </div>
-                                                    {/* little banner for the eco stuff */}
-                                                    <div className="py-3 text-center text-xs md:text-base">
-                                                        <div className="bg-rose-50 p-2 rounded-md">
-                                                            <div className="flex justify-center items-center">
-                                                                <img
-                                                                    src={getImageUrl('/images/icon-carbon-neutral.svg')}
-                                                                    className="h-full aspect-square object-cover" />
-                                                                <p className="pl-2">
-                                                                    This is a
-                                                                </p>
-                                                                <p className="font-bold px-1">
-                                                                    carbon-neutral
-                                                                </p>
-                                                                <p>
-                                                                    delivery
-                                                                </p>
-                                                            </div>
-                                                        </div>
-
-                                                    </div>
-                                                    {/* Confirm Order button */}
-                                                    <button
-                                                        onClick={() => { setIsModalOpen(true) }}
-                                                        className="bg-red rounded-full w-full h-full py-2 text-white">
-                                                        Confirm Order
-                                                    </button>
-                                                </div>
-                                            </>
-                                        }
                                     </div>
 
                                 </div>
-                            </>)}
-        </>
+                                {/* Confirm Order button */}
+                                <button
+                                    onClick={() => { setIsModalOpen(true) }}
+                                    className="bg-red rounded-full w-full h-full py-2 text-white">
+                                    Confirm Order
+                                </button>
+                            </div>
+                        </>
+                    }
+                </div>
 
+            </div>
+        </>
     )
 }
 
